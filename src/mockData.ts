dbcfe01e3d54cf3d3712e4dbdea1df54cde012b7
Seifeddine Ben Abda <42813983+SeifBenAbda@users.noreply.github.com>
2025-10-14 21:49:39 +0200

    Deleted file

--- src/mockData.ts
+++
@@ -1,5 +0,0 @@
-export const mockStats = [
-    { label: 'Total Earnings from Car Sales', value: '$25,000' },
-    { label: 'Total Earnings from Item Changes', value: '$5,000' },
-    { label: 'Total Orders', value: '150' },
-  ];
